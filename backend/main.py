105383105ccdfecdbcb81c4b4bb9b26d1053ba4c
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-03-27 06:43:50 +0300

    edit api show persons

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,20 @@
-from typing import Union
+from typing import List
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Person(BaseModel):
+    id: int
+    name: str
+    age: int
+    
+DB: List[Person] = [
+    Person(id=1,name="Ahmed", age=22),
+    Person(id=2,name="Hazim", age=23)
+]
 
-@app.get("/")
+@app.get("/api")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return DB
