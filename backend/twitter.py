c203a3dd285df48fcfe1466bc354174c6691d2f1
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-03-28 15:35:07 +0300

    get followers liked tweets, init

--- backend/twitter.py
+++ backend/twitter.py
@@ -1,4 +1,4 @@
-# import re
+import re
 import tweepy
 from conf import *
 
@@ -6,10 +6,13 @@ auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
+client = tweepy.Client(bearer_token=BEARER_TOKEN)
 
 def get_trends():
-    trends = api.get_place_trends(id = WEOID, exclude = "hashtags")
-    
+    return api.get_place_trends(id = WEOID, exclude = "hashtags")
+
+def test_trends():
+    trends = get_trends()   
     TRENDS = []
     # regexp = re.compile(r'[ء-ي]+')
     
@@ -21,5 +24,22 @@ def get_trends():
     return TRENDS
 
 
-trends = get_trends()
+def get_followers():
+    return api.get_followers(user_id = MY_ID)
+
+def get_liked_tweets(id):
+    return client.get_liked_tweets(id=id, tweet_fields=['context_annotations','created_at','geo'])
+
+def get_followers_liked_tweets():
+    for follower in get_followers():
+        print(follower.screen_name)
+        for tweet in get_liked_tweets(follower.id).data:
+            print(tweet.created_at)
+
+
+get_followers_liked_tweets()
+
+      
+def get_liked_tweets_by_followers():
+    pass
 
