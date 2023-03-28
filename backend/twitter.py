9e121fb0fd9edabfba4a09237f26922b2c7eda3b
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-03-28 13:30:51 +0300

    consider arabic text

--- backend/twitter.py
+++ backend/twitter.py
@@ -1,32 +1,25 @@
+import re
 import tweepy
 from conf import *
 
-client = tweepy.Client(bearer_token=BEARER_TOKEN)
-
-query = 'covid'
-
-res = client.search_recent_tweets(query=query)
-
-# print(res)
-
-# authorization of consumer key and consumer secret
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
- 
-# set access to user's access key and access secret
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
- 
-# calling the api
 api = tweepy.API(auth)
- 
-# WOEID of London
-woeid = 44418
- 
-# fetching the trends
-trends = api.get_place_trends(id = woeid)
- 
-# printing the information
-print("The top trends for the location are :")
- 
-for value in trends:
-    for trend in value['trends']:
-        print(trend['name'])
+
+
+def get_trends():
+    trends = api.get_place_trends(id = WEOID)
+    
+    TRENDS = []
+    regexp = re.compile(r'[ء-ي]+')
+    
+    for value in trends:
+        for trend in value['trends']:
+            trend_name = trend['name']
+            TRENDS.append(trend_name[::-1]) if regexp.search(trend_name) else TRENDS.append(trend_name)
+            
+    return TRENDS
+
+
+trends = get_trends()
+
