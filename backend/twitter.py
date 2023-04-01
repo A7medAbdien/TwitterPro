a47cf2f1428c3e806aa6bfcf43f81b3bdaaaaef0
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-01 05:45:51 +0300

    added replies data

--- backend/twitter.py
+++ backend/twitter.py
@@ -1,11 +1,16 @@
 import re
 from conf import *
 
+def get_user_info(user_id):
+    return client.get_user(
+        id = user_id,
+        user_fields = ['profile_image_url', 'created_at', 'public_metrics'])
+
 def get_trends():
     return api.get_place_trends(id = WOEID, exclude = "hashtags")
 
 def get_followers():
-    print(client.get_users_followers(ELUN_ID, max_results=FOLLOW_NUM))
+    return client.get_users_followers(ELUN_ID, max_results=FOLLOW_NUM)
 
 def get_following():
     return client.get_users_following(ELUN_ID, max_results=FOLLOW_NUM)
@@ -24,34 +29,39 @@ def get_liked_tweets(whom):
         try:
             tweets = get_user_liked_tweets(user.id).data
             if len(tweets) > 0:
-                for tweet in tweets:
-                    followers_liked_tweets[user.id] = {
-                        "id":tweet.id,
-                        "text":tweet.text,
-                        "created_at":tweet.created_at,
-                        "geo":tweet.geo,
-                        "public_metrics":tweet.public_metrics,
-                        "context_annotations":tweet.context_annotations
-                        }        
+                followers_liked_tweets[user.id] = user, tweets    
         except:
             print(f"This ID {user.id} {user.name} has no liked tweets")
             continue
-            
     return followers_liked_tweets
+
+def get_recent_tweets(id=ELUN_ID,max=TWEETS_NUM):
+    query_str = f"from:{id} -is:reply -is:retweet"
+    return client.search_recent_tweets(
+        query=query_str,
+        max_results = max,
+        tweet_fields= ['context_annotations','created_at','geo','public_metrics'])
+
+def get_recent_replies(id=ELUN_ID,max=TWEETS_NUM):
+    query_str = f"from:{id} is:reply -is:retweet"
+    return client.search_recent_tweets(
+        query=query_str,
+        max_results = max,
+        tweet_fields=['context_annotations','created_at','geo','public_metrics','in_reply_to_user_id'])
+    
+def get_top_tweets(id=ELUN_ID,max=TWEETS_NUM):
+  query_str = f"from:{id} -is:reply -is:retweet"
+  return client.search_recent_tweets(
+      query=query_str,
+      max_results = max,
+      sort_order="relevancy",
+      tweet_fields=['context_annotations','created_at','geo','public_metrics'])
 
 def extract_hashtags(followers_liked_tweets):
     tweets = {}
-    for user, tweet in followers_liked_tweets.items():
+    for user, tweet in followers_liked_tweets.items()[-1]:
         hashtags = list()
         for hashtag in re.findall(r"#(\w+)", tweet.text):
             hashtags.append(hashtag)
         if len(hashtags) > 0: tweets[user]= hashtags 
     return tweets
-
-def get_top_tweets(id , max = TWEETS_NUM):
-    query_str = f"from:{id} is:reply"
-    return client.search_recent_tweets(
-        query=query_str,
-        max_results = max,
-        sort_order="relevancy",
-        tweet_fields=['context_annotations','created_at', 'public_metrics','author_id', 'lang', 'geo', 'entities'])
