ac0c8e599188bf948039dcd48b28171f3f0135bd
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-09 05:30:49 +0300

    attempt to show mpld3 element

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+import React from 'react';
+import InnerHTML from 'dangerously-set-html-content'
 import './App.css';
 
 function App() {
@@ -8,11 +10,18 @@ function App() {
   useEffect(() => {
     axios.get('/api').then(res => setPeople(res.data))
     // axios.get('/api')
+
   }, [])
 
-  return people.map((p, i) => {
-    return <p key={i}> {p.id} {p.name} {p.age}</p>
-  });
+  const html = ``
+  console.log(people);
+  return (
+    <div>
+      hi
+      <InnerHTML html={html} />
+    </div>
+  )
 }
 
 export default App;
+
