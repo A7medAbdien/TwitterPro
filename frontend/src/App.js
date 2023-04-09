5f8ebc1e021b7e0558b86989e1de6f5882727fc6
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-09 06:24:00 +0300

    using jquery in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,32 @@
 // import axios from 'axios'
-// import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import React from 'react';
 // import InnerHTML from 'dangerously-set-html-content'
 import './App.css';
+import $ from 'jquery'
 
 function App() {
+  const [random, setRandom] = useState();
+
+  const getRandom = () => {
+    $.ajax({
+      url: "http://127.0.0.1:8000/random/10",
+      type: 'GET',
+      dataType: 'json',
+      success: (data) => {
+        setRandom(data)
+      }
+    })
+  }
+
+  useEffect(() => {
+    getRandom();
+  }, [])
 
   return (
     <div>
       hi
+      {random}
     </div>
   )
 }
