0737683b525e0db8a50a995092dbd8ac846e1323
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-09 10:56:07 +0300

    BarChar comp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react'
 import React from 'react';
-// import './App.css';
 import $ from 'jquery'
-import Plot from 'react-plotly.js';
+import BarChar from './components/BarChar';
 
 function App() {
   const [random, setRandom] = useState([]);
@@ -22,40 +21,14 @@ function App() {
     getRandom();
   }, [])
 
-  return (
-    <Plot
-      data={[
-        {
-          x: random[0],
-          y: random[1],
-          type: 'bar',
-          text: random[1],
-          textfont: {
-            family: 'Arial',
-            size: 16,
-            color: '#fcfcfc'
-          }
-        }
-      ]}
-      layout={
-        {
-          title: 'Histogram API',
-          xaxis: {
-            title: 'Random Numbers',
-            tickvals: random[0],
-            ticktext: random[0]
-          },
-          yaxis: {
-            title: 'Frequency',
-            tickmode: 'linear',
-            dtick: 1,
-            tickformat: 'd'
-          },
-          bargap: 0.2
-        }
-      }
+  return <>
+    <BarChar
+      data={random}
+      title={"title"}
+      xLabel={"xLabel"}
+      yLabel={"yLabel"}
     />
-  )
+  </>
 }
 
 export default App;
---
+++ frontend/src/components/BarChar.jsx
@@ -0,0 +1,44 @@
+import Plot from 'react-plotly.js';
+
+const BarChar = ({ data, title, xLabel, yLabel }) => {
+
+    const [x, y] = data
+
+    return (
+        <Plot
+            data={[
+                {
+                    x: x,
+                    y: y,
+                    type: 'bar',
+                    text: y,
+                    textfont: {
+                        family: 'Arial',
+                        size: 16,
+                        color: '#fcfcfc'
+                    }
+                }
+            ]}
+            layout={
+                {
+                    title: title,
+                    xaxis: {
+                        title: xLabel,
+                        tickvals: x,
+                        ticktext: x
+                    },
+                    yaxis: {
+                        title: yLabel,
+                        tickmode: 'linear',
+                        dtick: 1,
+                        tickformat: 'd'
+                    },
+                    bargap: 0.2
+                }
+            }
+        />
+    )
+}
+
+export default BarChar;
+
