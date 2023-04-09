cc2d6a21882ed3db29da74316919726400625d52
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-09 10:40:18 +0300

    clean 🧹

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,5 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Experience from './Experience.jsx'
 import App from './App.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
@@ -9,15 +7,5 @@ const root = ReactDOM.createRoot(document.querySelector('#root'))
 root.render(
     <>
         <App />
-        {/* <Canvas
-            camera={{
-                fov: 45,
-                near: 0.1,
-                far: 200,
-                position: [4, - 2, 6]
-            }}
-        >
-            <Experience />
-        </Canvas> */}
     </>
 )
