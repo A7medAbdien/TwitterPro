1fae8957d8791aac16611c9dea85c0308777daef
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-14 12:49:30 +0300

    separate Canvas from gallery

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -9,29 +9,28 @@ import getUuid from 'uuid-by-string'
 const GOLDENRATIO = 1.3
 
 export const Gallery = ({ images }) => (
-    <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
-        <color attach="background" args={['#191920']} />
-        <fog attach="fog" args={['#191920', 0, 15]} />
-        <group position={[0, -0.5, 0]}>
-            <Frames images={images} />
-            <mesh rotation={[-Math.PI / 2, 0, 0]}>
-                <planeGeometry args={[50, 50]} />
-                <MeshReflectorMaterial
-                    blur={[300, 100]}
-                    resolution={2048}
-                    mixBlur={1}
-                    mixStrength={50}
-                    roughness={1}
-                    depthScale={1.2}
-                    minDepthThreshold={0.4}
-                    maxDepthThreshold={1.4}
-                    color="#050505"
-                    metalness={0.5}
-                />
-            </mesh>
-        </group>
-        <Environment preset="city" />
-    </Canvas>
+
+    <group position={[0, -0.5, 0]}>
+
+        <Frames images={images} />
+
+        <mesh rotation={[-Math.PI / 2, 0, 0]}>
+            <planeGeometry args={[50, 50]} />
+            <MeshReflectorMaterial
+                blur={[300, 100]}
+                resolution={2048}
+                mixBlur={1}
+                mixStrength={50}
+                roughness={1}
+                depthScale={1.2}
+                minDepthThreshold={0.4}
+                maxDepthThreshold={1.4}
+                color="#050505"
+                metalness={0.5}
+            />
+        </mesh>
+    </group>
+
 )
 
 function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() }) {
@@ -78,8 +77,10 @@ function Frame({ url, c = new THREE.Color(), ...props }) {
         easing.damp3(image.current.scale, [0.85 * (!isActive && hovered ? 0.85 : 1), 0.9 * (!isActive && hovered ? 0.905 : 1), 1], 0.1, dt)
         easing.dampC(frame.current.material.color, hovered ? 'orange' : 'white', 0.1, dt)
     })
+
     return (
         <group {...props}>
+            {/* Outer Frame */}
             <mesh
                 name={name}
                 onPointerOver={(e) => (e.stopPropagation(), hover(true))}
@@ -88,12 +89,18 @@ function Frame({ url, c = new THREE.Color(), ...props }) {
                 position={[0, GOLDENRATIO / 2, 0]}>
                 <boxGeometry />
                 <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
+
+                {/* Inner Frame */}
                 <mesh ref={frame} raycast={() => null} scale={[0.9, 0.93, 0.9]} position={[0, 0, 0.2]}>
                     <boxGeometry />
                     <meshBasicMaterial toneMapped={false} fog={false} />
                 </mesh>
+
+                {/* Image */}
                 <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url} />
             </mesh>
+
+            {/* Title */}
             <Text maxWidth={1} anchorX="left" anchorY="top" position={[-0.45, 0.1 + GOLDENRATIO, 0]} fontSize={0.05}>
                 {name.split('-').join(' ')}
             </Text>
