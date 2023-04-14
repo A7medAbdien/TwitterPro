afae87a55d02d4f62caebb1f2ea9f5ad3776869c
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-14 09:21:56 +0300

    start three js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,12 @@
-import { useState, useEffect, useRef } from 'react'
 import React from 'react';
-import { FAll, FTweets, FReplies } from './components/Freq';
-import { getComm, getTermFreqUni, getTermFreqBi, getUserFreq, getTopicFreq, getTimeFreq } from './api';
-import { TimeReplies, TimeTweets } from './components/TimeHeatmap';
-import { VComm } from './components/VComm';
-import { TwoBar } from './components/charts/TwoBar';
-import $ from 'jquery'
-
-const URL = "http://127.0.0.1:8000"
+import Experience from './Experience';
+import { Canvas } from '@react-three/fiber';
 
 function App() {
-  // const refs = useRef([])
-  const [termFreqUni, setTermFreqUni] = useState([]);
-  const [topic, setTopic] = useState([]);
-  const [timeFreq, setTimeFreq] = useState([]);
-  const [comm, setComm] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-
-  useEffect(() => {
-    Promise.all([
-      getTermFreqUni(setTermFreqUni),
-      getTopicFreq(setTopic),
-      getTimeFreq(setTimeFreq),
-      getComm(setComm),
-    ]).then(() => {
-      setIsLoading(false)
-      console.log(comm);
-    })
-  }, [])
-
-
   return <>
-    <TwoBar {...comm.uni_term} />
-    <VComm {...comm.user_rl} />
-    <FTweets res={termFreqUni} />
-    <FTweets res={topic} />
-    <TimeTweets res={timeFreq} />
+    <Canvas>
+      <Experience />
+    </Canvas>
   </>
 }
 
--- frontend/src/Experience.jsx
+++ frontend/src/Experience.jsx
@@ -1,15 +1,51 @@
-import { OrbitControls } from '@react-three/drei'
+import { Html, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 
-export default function Experience()
-{
+import { useState, useEffect, useRef } from 'react'
+
+import { FAll, FTweets, FReplies } from './components/Freq';
+import { getComm, getTermFreqUni, getTermFreqBi, getUserFreq, getTopicFreq, getTimeFreq } from './api';
+import { TimeReplies, TimeTweets } from './components/TimeHeatmap';
+import { VComm } from './components/VComm';
+import { TwoBar } from './components/charts/TwoBar';
+
+export default function Experience() {
+
+    const [termFreqUni, setTermFreqUni] = useState([]);
+    const [topic, setTopic] = useState([]);
+    const [timeFreq, setTimeFreq] = useState([]);
+    const [comm, setComm] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    useEffect(() => {
+        Promise.all([
+            getTermFreqUni(setTermFreqUni),
+            getTopicFreq(setTopic),
+            getTimeFreq(setTimeFreq),
+            getComm(setComm),
+        ]).then(() => {
+            setIsLoading(false)
+            console.log(comm);
+        })
+    }, [])
+
+
     return <>
+        {/* <TwoBar {...comm.uni_term} />
+        <VComm {...comm.user_rl} />
+        <FTweets res={termFreqUni} />
+        <FTweets res={topic} />
+        <TimeTweets res={timeFreq} /> */}
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 }>
+        <Html>
+            <VComm {...comm.user_rl} />
+        </Html>
+        <mesh scale={1.5}>
             <boxGeometry />
             <meshNormalMaterial />
         </mesh>
