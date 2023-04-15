8fb28f715a1575c60f0af30cd148ccabc8f7854b
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-15 13:50:30 +0300

    stop back onDoubleClick, change Z pos based insted

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -6,8 +6,14 @@ import { useRoute, useLocation } from 'wouter'
 import { easing } from 'maath'
 import getUuid from 'uuid-by-string'
 import { VComm } from './VComm'
+import { useControls } from 'leva'
 
-const GOLDENRATIO = 1.3
+const focusedDistance = 1.5
+
+const Hight = 1.3
+
+const OuterFrameWidth = 1
+const OuterFrameZ = 0.05
 
 export const Gallery = ({ images }) => (
 
@@ -39,13 +45,21 @@ function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() })
     const clicked = useRef()
     const [, params] = useRoute('/item/:id')
     const [, setLocation] = useLocation()
+    const z = (ref, n) => {
+        if (ref.current) {
+            const chart = ref.current.getObjectByName('test')
+            chart.position.z = n
+        }
+    }
     useEffect(() => {
         clicked.current = ref.current.getObjectByName(params?.id)
         if (clicked.current) {
             clicked.current.parent.updateWorldMatrix(true, true)
-            clicked.current.parent.localToWorld(p.set(0, GOLDENRATIO / 2, 1.25))
+            clicked.current.parent.localToWorld(p.set(0, Hight / 2, focusedDistance))
             clicked.current.parent.getWorldQuaternion(q)
-        } else {
+            z(clicked, 1)
+        }
+        else {
             p.set(0, 0, 5.5)
             q.identity()
         }
@@ -57,14 +71,15 @@ function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() })
     return (
         <group
             ref={ref}
-            onClick={(e) => (e.stopPropagation(), setLocation(clicked.current === e.object ? '/' : '/item/' + e.object.name))}
-            onPointerMissed={() => setLocation('/')}>
+            onClick={(e) => (e.stopPropagation(), setLocation(!clicked.current === e.object ? '/' : '/item/' + e.object.name))}
+            onPointerMissed={() => (setLocation('/'), z(clicked, -1))}>
             {images.map((props) => <Frame key={props.url} {...props} /> /* prettier-ignore */)}
         </group>
     )
 }
 
 function Frame({ url, c = new THREE.Color(), ...props }) {
+    const [hidden, set] = useState()
     const d = props.d
     const image = useRef()
     const frame = useRef()
@@ -87,8 +102,8 @@ function Frame({ url, c = new THREE.Color(), ...props }) {
                 name={name}
                 onPointerOver={(e) => (e.stopPropagation(), hover(true))}
                 onPointerOut={() => hover(false)}
-                scale={[1, GOLDENRATIO, 0.05]}
-                position={[0, GOLDENRATIO / 2, 0]}>
+                scale={[OuterFrameWidth, Hight, OuterFrameZ]}
+                position={[0, Hight / 2, 0]}>
                 <boxGeometry />
                 <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
 
@@ -100,15 +115,26 @@ function Frame({ url, c = new THREE.Color(), ...props }) {
 
                 {/* Image */}
                 <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url} />
-                <Html position={[0.5, GOLDENRATIO, 0]}>
-                    <VComm {...d} />
+                <Html
+                    visible={false}
+                    occlude
+                    onOcclude={set}
+                    style={{
+                        transition: 'all  1s',
+                        opacity: hidden ? 0 : 1,
+                        transform: `scale(${hidden ? 0.5 : 1})`
+                    }}
+                    name='test'
+                    position={[0, 0, 0]}>
+                    {/* <VComm {...d} /> */}
+                    {d}
                 </Html>
             </mesh>
 
             {/* Title */}
-            <Text maxWidth={1} anchorX="left" anchorY="top" position={[-0.45, 0.1 + GOLDENRATIO, 0]} fontSize={0.05}>
+            <Text maxWidth={1} anchorX="left" anchorY="top" position={[-0.45, 0.1 + Hight, 0]} fontSize={0.05}>
                 {name.split('-').join(' ')}
             </Text>
-        </group>
+        </group >
     )
 }
