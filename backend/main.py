8c7c895fd5a7a7e5bc865e588c08471750095ecc
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-17 21:35:18 +0300

    send charts

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 
 import numpy as np
-
+import sys
 from analysis import *
 from strings import *
+from matplotlib.ticker import MaxNLocator
 from twitterAPI import get_user_info, USERNAME
 import mpld3 as mp
 
@@ -26,6 +27,45 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+import matplotlib
+import seaborn as sns
+import matplotlib.pyplot as plt
+import io
+
+"""
+Test
+"""
+
+
+class RandomNumbers:
+    def __init__(self, n):
+        self.n = n
+        self.data = np.random.randint(0, 100, n)
+
+    def get_data(self):
+        return self.data.tolist()
+
+    def add(self, n):
+        self.data = np.append(self.data, np.random.randint(0, 100, n))
+
+
+@app.get("/hist/{n}",
+         responses={
+             404: {"description": "Not Found"},
+             200: {"content": {"image/png": {}}}
+         })
+async def hist(n: int):
+    r = RandomNumbers(n)
+    fig = sns.histplot(r.get_data(), bins=10).get_figure()
+    with io.BytesIO() as fig_bytes:
+        sys.stdout.flush()
+        fig.savefig(fig_bytes, format="png")
+        fig_bytes.seek(0)
+        response = Response(fig_bytes.getvalue(), media_type='image/png')
+        plt.close()
+    return response
+
+
 term_freq_uni_data = {
     key: {
         "data": data,
@@ -34,6 +74,34 @@ term_freq_uni_data = {
         "yLabel": "Frequency"
     } for key, data in term_freq_uni_data.items()
 }
+
+
+# for key, data in term_freq_uni_data.items():
+def plot_term_freq_uni(data, title, x_label, y_label):
+    ax = plt.figure(figsize=(15, 5)).gca()
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    # creating the bar plot
+    plt.bar(data[0], data[1], color='b', width=0.4)
+
+    plt.xlabel("Terms")
+    plt.ylabel("Frequency")
+    plt.title(f'Terms Frequencies')
+    plt.xticks(rotation=60)
+    with io.BytesIO() as fig_bytes:
+        sys.stdout.flush()
+        plt.savefig(fig_bytes, format="png")
+        fig_bytes.seek(0)
+        response = Response(fig_bytes.getvalue(), media_type='image/png')
+        plt.close()
+        return response
+
+
+df = term_freq_uni_data['tweets']
+
+@app.get("/ch/tf/uni/1")
+async def tf_uni():
+    return plot_term_freq_uni(df['data'], df['title'], df['xLabel'], df['yLabel'])
 
 
 @app.get("/ch/tf/uni")
