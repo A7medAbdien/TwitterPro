e175b9bff612a998dac96e3ac2a66255d61b201d
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-17 16:25:02 +0300

    set poses

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,8 @@ const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}
 function App() {
 
   const [termFreqUni, setTermFreqUni] = useState([]);
+  const [termFreqBi, setTermFreqBi] = useState([]);
+  const [user, setUser] = useState([]);
   const [topic, setTopic] = useState([]);
   const [timeFreq, setTimeFreq] = useState([]);
   const [comm, setComm] = useState([]);
@@ -27,11 +29,14 @@ function App() {
   useEffect(() => {
     Promise.all([
       getTermFreqUni(setTermFreqUni),
+      getTermFreqBi(setTermFreqBi),
       getTopicFreq(setTopic),
+      getUserFreq(setUser),
       getTimeFreq(setTimeFreq),
       getComm(setComm),
     ]).then(() => {
-      // console.log(termFreqUni);
+      console.log(termFreqUni);
+      console.log(termFreqBi);
       setIsLoading(false)
     })
   }, [])
@@ -57,19 +62,24 @@ function App() {
   const termFreqUniRoom = [
     // Front
     // { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970), data: comm.user_rl, type: "venn" },
-    // Back
-    { position: [-1.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430), data: termFreqUni.tweets, type: "bar" },
-    { position: [1.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452), data: termFreqUni.tweets, type: "bar" },
+
+
 
     // // Left
-    // { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482) },
-    // { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185) },
-    // { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574) },
-    // // Right
-    // { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675) },
-    // { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738) },
-    // { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986) }
+    // Back
+    { position: [-5, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430), data: termFreqUni.tweets, type: "bar" },
+    { position: [-2, 0, -0.6], rotation: [0, 0, 0], url: pexel(16207048), data: termFreqUni.replies, type: "bar" },
 
+    { position: [-6.5, 0, 1], rotation: [0, Math.PI / 2, 0], url: pexel(2603464), data: termFreqUni.likedTweets, type: "bar" },
+    { position: [-6.5, 0, 4], rotation: [0, Math.PI / 2, 0], url: pexel(325185), data: termFreqUni.followingLiked, type: "bar" },
+
+    // // Right
+    // Back
+    { position: [2, 0, -0.6], rotation: [0, 0, 0], url: pexel(227675), data: termFreqBi.tweets, type: "bar" },
+    { position: [5, 0, -0.6], rotation: [0, 0, 0], url: pexel(16350045), data: termFreqBi.replies, type: "bar" },
+
+    { position: [6.5, 0, 1], rotation: [0, -Math.PI / 2, 0], url: pexel(1738986), data: termFreqBi.likedTweets, type: "bar" },
+    { position: [6.5, 0, 4], rotation: [0, -Math.PI / 2, 0], url: pexel(10401968), data: termFreqBi.followingLiked, type: "bar" },
   ]
 
   return <>
