d2c3abb49e4fd512673b31f9428035be26ad8f5f
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-17 20:46:39 +0300

    new BASE, only image no plotly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import Experience from './Experience';
 import { Canvas, useThree } from '@react-three/fiber';
-import { Room } from './components/Gallery';
+import { Gallery } from './components/Gallery';
 import { Html, Environment, } from '@react-three/drei'
 import { Leva } from 'leva'
 
@@ -43,19 +43,19 @@ function App() {
 
   const images = [
     // Front
-    { position: [-1.5, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970), data: comm.user_rl, type: "venn" },
+    { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
     // Back
-    { position: [1.5, 0, 1.5], rotation: [0, 0, 0], url: pexel(416430), data: termFreqUni.tweets, type: "bar" },
-    // { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
+    { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
+    { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
+    // Left
+    { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482) },
+    { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185) },
+    { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574) },
+    // Right
+    { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675) },
+    { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738) },
+    { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986) }
 
-    // // Left
-    // { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482) },
-    // { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185) },
-    // { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574) },
-    // // Right
-    // { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675) },
-    // { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738) },
-    // { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986) }
 
   ]
 
@@ -84,7 +84,7 @@ function App() {
       <fog attach="fog" args={['#191920', 0, 15]} />
 
       <Experience />
-      <Room images={termFreqUniRoom} />
+      <Gallery images={images} />
       <Environment preset="city" />
     </Canvas>
   </>
