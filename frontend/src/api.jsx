580b3b8f63c009991e01690135fd074238f1fcab
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-18 12:57:17 +0300

    assign ajax promise- base

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -2,69 +2,69 @@ import $ from 'jquery'
 
 const URL = "http://127.0.0.1:8000"
 
-export const getTermFreqUni = (set) => {
+export const getTermFreqUni =
     $.ajax({
         url: `${URL}/ch/tf/uni`,
         type: 'GET',
         dataType: 'json',
         success: (res) => {
-            set(res)
+            return res
         }
     })
-}
 
-export const getTermFreqBi = (set) => {
+
+export const getTermFreqBi =
     $.ajax({
         url: `${URL}/ch/tf/bi`,
         type: 'GET',
         dataType: 'json',
         success: (res) => {
-            set(res)
+            return res
         }
     })
-}
 
-export const getUserFreq = (set) => {
+
+export const getUserFreq =
     $.ajax({
         url: `${URL}/ch/user`,
         type: 'GET',
         dataType: 'json',
         success: (res) => {
-            set(res)
+            return res
         }
     })
-}
 
-export const getTopicFreq = (set) => {
+
+export const getTopicFreq =
     $.ajax({
         url: `${URL}/ch/topic`,
         type: 'GET',
         dataType: 'json',
         success: (res) => {
-            set(res)
+            return res
         }
     })
-}
 
-export const getTimeFreq = (set) => {
+
+export const getTimeFreq =
     $.ajax({
         url: `${URL}/ch/time`,
         type: 'GET',
         dataType: 'json',
         success: (res) => {
-            set(res)
+            return res
         }
     })
-}
 
-export const getComm = (set) => {
+
+export const getComm =
     $.ajax({
         url: `${URL}/ch/comm`,
         type: 'GET',
         dataType: 'json',
         success: (res) => {
-            set(res)
+            return res
         }
     })
-}
+
 
