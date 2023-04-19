af1c06300b4db867f84186a0b034137b84591269
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-20 01:20:30 +0300

    trolling XD

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import Experience from './Experience'
-import { Canvas, useThree } from '@react-three/fiber'
+import { Canvas, extend, useFrame, useThree } from '@react-three/fiber'
+import { Physics, useSphere } from "@react-three/cannon"
+import * as THREE from "three"
+
 import { Gallery } from './components/Gallery'
-import { Html, Environment, OrbitControls } from '@react-three/drei'
+import { Html, Environment, OrbitControls, MeshReflectorMaterial } from '@react-three/drei'
 import { Leva } from 'leva'
 
 import { useState, useEffect, useRef, useMemo } from 'react'
@@ -58,18 +61,64 @@ function App() {
   return <>
 
     {/* <Leva hidden /> */}
-    <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
+    <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 10] }}>
       {/* <OrbitControls makeDefault /> */}
       <color attach="background" args={['#fff']} />
       <fog attach="fog" args={['#191920', 0, 15]} />
-
       <Experience />
       {!isLoading && (
         <Gallery images={images} />
       )}
+
+      <Physics gravity={[0, 2, 0]} iterations={10}>
+        <Pointer />
+        <Clump />
+      </Physics>
+
+      <mesh position={[0, -0.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[50, 50]} />
+        <MeshReflectorMaterial
+          blur={[300, 100]}
+          resolution={2048}
+          mixBlur={1}
+          mixStrength={50}
+          roughness={1}
+          depthScale={1.2}
+          minDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          color="#050505"
+          metalness={0.5}
+        />
+      </mesh>
       <Environment preset="city" />
     </Canvas>
   </>
+}
+
+
+const rfs = THREE.MathUtils.randFloatSpread
+const sphereGeometry = new THREE.SphereGeometry(0.5, 12, 12)
+const baubleMaterial = new THREE.MeshMatcapMaterial({ color: "red", })
+
+function Clump({ mat = new THREE.Matrix4(), vec = new THREE.Vector3(), ...props }) {
+  // const texture = useTexture("/cross.jpg")
+  const [ref, api] = useSphere(() => ({ args: [0.5], mass: 1, angularDamping: 0.1, linearDamping: 0.75, position: [rfs(20), rfs(20), rfs(20)] }))
+  useFrame((state) => {
+    for (let i = 0; i < 30; i++) {
+      // Get current whereabouts of the instanced sphere
+      ref.current.getMatrixAt(i, mat)
+      // Normalize the position and multiply by a negative force.
+      // This is enough to drive it towards the center-point.
+      api.at(i).applyForce(vec.setFromMatrixPosition(mat).normalize().multiplyScalar(-50).toArray(), [3, 3, 3])
+    }
+  })
+  return <instancedMesh position={[0, 0, -2]} scale={0.15} ref={ref} args={[null, null, 30]} geometry={sphereGeometry} material={baubleMaterial} />
+}
+
+function Pointer() {
+  const viewport = useThree((state) => state.viewport)
+  const [, api] = useSphere(() => ({ type: "Kinematic", args: [1], position: [0, 0, 0] }))
+  return useFrame((state) => api.position.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 0))
 }
 
 export default App
--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -18,21 +18,7 @@ export const Gallery = ({ images }) => (
 
         <Frames images={images} />
 
-        <mesh rotation={[-Math.PI / 2, 0, 0]}>
-            <planeGeometry args={[50, 50]} />
-            <MeshReflectorMaterial
-                blur={[300, 100]}
-                resolution={2048}
-                mixBlur={1}
-                mixStrength={50}
-                roughness={1}
-                depthScale={1.2}
-                minDepthThreshold={0.4}
-                maxDepthThreshold={1.4}
-                color="#050505"
-                metalness={0.5}
-            />
-        </mesh>
+
     </group>
 
 )
