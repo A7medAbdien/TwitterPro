7258c2ab61a53c5d658dd21f765b93335033f7ce
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-20 11:58:40 +0300

    separate Floor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,7 @@ import { TwoBar } from './components/charts/TwoBar'
 import BarChar from './components/charts/Bar'
 import { assignUrlToImage, getBarUrl, getUrlFromData } from './components/Helpers/UrlChart'
 import { Clump, Pointer } from './components/Clump'
+import { Floor } from './components/Floor'
 
 const x = 0
 const z = -4
@@ -76,21 +77,8 @@ function App() {
         <Clump />
       </Physics>
 
-      <mesh position={[0, -0.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-        <planeGeometry args={[50, 50]} />
-        <MeshReflectorMaterial
-          blur={[300, 100]}
-          resolution={2048}
-          mixBlur={1}
-          mixStrength={50}
-          roughness={1}
-          depthScale={1.2}
-          minDepthThreshold={0.4}
-          maxDepthThreshold={1.4}
-          color="#050505"
-          metalness={0.5}
-        />
-      </mesh>
+      <Floor />
+
       <Environment preset="city" />
     </Canvas>
   </>
