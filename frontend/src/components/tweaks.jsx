f87d606b65ebf46ba928b4e0a0105e90bebc1af7
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-04-21 19:28:26 +0300

    common topics Room done

--- frontend/src/components/tweaks.jsx
+++ frontend/src/components/tweaks.jsx
@@ -73,7 +73,6 @@ const termFreq = () => {
         { img: 0, meta: 'fLikes', position: [positionD.x + posTF.x, 0, positionD.y + posTF.y], rotation: [0, -rD, 0], url: 'TFUfLikes2' },
     ]
 }
-
 const topicsTime = () => {
     const { position: posTF, positionA, rA, positionC, rC, positionD, rD } = useControls('TT', {
         position:
@@ -130,6 +129,64 @@ const topicsTime = () => {
         // // Right
         { img: 0, meta: 'likes', position: [-positionD.x + posTF.x, 0, positionD.y + posTF.y], rotation: [0, rD, 0], url: 'TFBLikes2' },
         { img: 0, meta: 'fLikes', position: [positionD.x + posTF.x, 0, positionD.y + posTF.y], rotation: [0, -rD, 0], url: 'TFBFLikes2' },
+    ]
+}
+const commonTopics = () => {
+    const { position: posTF, positionA, rA, positionC, rC, positionD, rD } = useControls('Common Topics', {
+        position:
+        {
+            value: { x: 0, y: -6.5 },
+            step: 0.1,
+        },
+        positionA:
+        {
+            value: { x: 1.1, y: 0.2 },
+            step: 0.1,
+        },
+        rA:
+        {
+            min: -Math.PI,
+            max: Math.PI,
+            step: 0.05,
+            value: 0,
+        },
+        positionC:
+        {
+            value: { x: 2.7, y: 1.3 },
+            step: 0.1,
+        },
+        rC:
+        {
+            min: -Math.PI,
+            max: Math.PI,
+            step: 0.05,
+            value: 1.05,
+        },
+        positionD:
+        {
+            value: { x: 3.4, y: 3.5 },
+            step: 0.1,
+        },
+        rD:
+        {
+            min: -Math.PI,
+            max: Math.PI,
+            step: 0.05,
+            value: 1.25,
+        },
+    })
+
+    return [
+        // Back
+        { img: 0, meta: 'tweets', position: [-positionA.x + posTF.x, 0, positionA.y + posTF.y], rotation: [0, rA, 0], url: 'CTTweets' },
+        { img: 0, meta: 'replies', position: [positionA.x + posTF.x, 0, positionA.y + posTF.y], rotation: [0, -rA, 0], url: 'CTReplies' },
+
+        // Left
+        { img: 0, meta: 'likes', position: [-positionC.x + posTF.x, 0, positionC.y + posTF.y], rotation: [0, rC, 0], url: 'CTLikes' },
+        { img: 0, meta: 'fLikes', position: [positionC.x + posTF.x, 0, positionC.y + posTF.y], rotation: [0, -rC, 0], url: 'CTFLikes' },
+        // // Right
+        { img: 0, meta: 'likes', position: [-positionD.x + posTF.x, 0, positionD.y + posTF.y], rotation: [0, rD, 0], url: 'CTLikes2' },
+        { img: 0, meta: 'fLikes', position: [positionD.x + posTF.x, 0, positionD.y + posTF.y], rotation: [0, -rD, 0], url: 'CTFLikes2' },
     ]
 }
 const users = () => {
@@ -231,14 +288,14 @@ export const imagess = () => {
             { img: 0, meta: 'home', position: [0, 0, posOuterDoor.y], rotation: [-Math.PI / 2, 0, 0], url: 'home' },
             ]
         },
-        // {
-        //     position: [-posOuterDoor.x, 0, posOuterDoor.y],
-        //     rotation: [0, -rOuterDoor, 0],
-        //     image: [
-        //     { img: 0, meta: 'door', position: [0, 0, 5], rotation: [0, 0, 0], url: 'door-termFreqBi' },
-        //     { img: 0, meta: 'home', position: [0, 0, posOuterDoor.y], rotation: [-Math.PI / 2, 0, 0], url: 'home' },
-        //     ]
-        // },
+        {
+            position: [-posOuterDoor.x, 0, posOuterDoor.y],
+            rotation: [0, -rOuterDoor, 0],
+            image: [...commonTopics(),
+            { img: 0, meta: 'door', position: [0, 0, 5], rotation: [0, 0, 0], url: 'door-CT' },
+            { img: 0, meta: 'home', position: [0, 0, -2.5], rotation: [-Math.PI / 2, 0, 0], url: 'home' },
+            ]
+        },
         {
             position: [posInnerDoor.x, 0, posInnerDoor.y],
             rotation: [0, rInnerDoor, 0],
